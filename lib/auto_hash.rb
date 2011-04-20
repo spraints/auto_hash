e312ad4a3307520d58c55278aba3560c45f299ee
Matt Burke <spraints@gmail.com>
2011-04-20 12:34:02 -0400

    Don't auto-create hash keys on read.

--- lib/auto_hash.rb
+++ lib/auto_hash.rb
@@ -1,5 +1,33 @@
 class AutoHash < Hash
   def initialize
-    super { |h,k| h[k] = AutoHash.new }
+    super { |h,k| MaybeNestedHash.new(h, k) }
+  end
+
+  class BlankSlate
+    instance_methods.each { |m| undef_method m unless m =~ /^__/ }
+  end
+
+  class MaybeNestedHash < BlankSlate
+    def initialize(hash, key)
+      @hash = hash
+      @key = key
+      @delegate = nil
+    end
+
+    def [](key)
+      MaybeNestedHash.new(self, key)
+    end
+
+    def []=(key, value)
+      if @delegate.nil?
+        @delegate = AutoHash.new
+        @hash[@key] = @delegate
+      end
+      @delegate[key] = value
+    end
+
+    def method_missing(m, *args, &block)
+      @delegate.__send__(m, *args, &block)
+    end
   end
 end
--- spec/auto_hash_spec.rb
+++ spec/auto_hash_spec.rb
@@ -2,8 +2,13 @@ require 'auto_hash'
 
 describe AutoHash do
   it 'creates deep hashes' do
+    debugger
     subject['a']['b']['c'] = 3
     subject.should == { 'a' => { 'b' => { 'c' => 3 } } }
+  end
+
+  it 'returns nil for unassigned members' do
+    subject['a'].should be_nil
   end
 end
 
